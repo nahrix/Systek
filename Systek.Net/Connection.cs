06aab4997b080c1868ea756d34b5792d51f2fced
Ryan Duff <ryan.duff@responsetek.com>
2017-01-06 14:23:33 -0800

    Sync code fix.

--- Systek.Net/Connection.cs
+++ Systek.Net/Connection.cs
@@ -261,6 +261,15 @@ namespace Systek.Net
             }
         }
 
+        /// <summary>
+        /// Sends a message synchronously.  Returns when a reply is received, or when the
+        /// <see cref="Systek.Net.IConnection.Timeout" /> period has expired.
+        /// </summary>
+        /// <param name="msg">The <see cref="Systek.Net.Message" /> to send.</param>
+        /// <returns>
+        /// The reply <see cref="Systek.Net.Message" />.  <see cref="Systek.Net.Message.Type" /> will
+        /// be set to TIMEOUT is the message has timed out, or FAIL if the send function threw an exception.
+        /// </returns>
         public Message SendSync(Message msg)
         {
             Message reply = new Message();
@@ -290,6 +299,7 @@ namespace Systek.Net
                     reply.Msg = "Reply timed out.";
                 }
 
+                _SynchronizedMessages.TryRemove(msg.SyncId, out msg);
                 return reply;
             }
             catch (Exception e)
